cde9a074a2ed44d0105520fa83dc95d13ad15382
mageia <yzg96@gmail.com>
2015-10-19 15:56:55 +0800

    avoscloud configure

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
-from flask import Flask, render_template
+# coding: utf-8
+
+from datetime import datetime
+
+from flask import Flask
+from flask import render_template
+from views.todos import todos_view
 from views.panel import panel
 from views.dashboard import dashboard
 from views.dash_source import dash_source
 from views.integration import integration
 from views.settings import settings
-import leancloud
-
 
 app = Flask(__name__)
+
+# 动态路由
+app.register_blueprint(todos_view, url_prefix='/todos')
 app.register_blueprint(panel)
 app.register_blueprint(dashboard)
 app.register_blueprint(dash_source)
@@ -15,19 +22,12 @@ app.register_blueprint(integration)
 app.register_blueprint(settings)
 
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-if __name__ == '__main__':
-    leancloud.init('z6fhqxvpal43l238q7xzogfdls74my214o5bapm5vkwfn4xh',
-                   'rb7jufb22o15nzc9ub5b6b0lx3xt845o2ofz494oc1s9esg8')
-    app.run(debug=True)
-
-
-
-
-
-
-
+@app.route('/time')
+def time():
+    return str(datetime.now())
