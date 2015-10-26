ec35cf8a4fea4da1ccafc34b648bfb8e89678a5b
mageia <yzg96@gmail.com>
2015-10-26 20:21:44 +0800

    start.sh

--- start.sh
+++ start.sh
@@ -4,4 +4,5 @@ export LC_APP_KEY=rb7jufb22o15nzc9ub5b6b0lx3xt845o2ofz494oc1s9esg8
 export LC_APP_MASTER_KEY=nnh0isf14u75x4ju6aeydey71drsla2mlxvcyps8ar0vf2ne
 export LC_APP_PORT=3000
 
+
 python wsgi.py
