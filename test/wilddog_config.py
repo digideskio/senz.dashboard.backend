771d71352a79d51e047dbaeb97471327aacb9cc2
mageia <yzg963@gmail.com>
2015-11-06 00:27:00 +0800

    post wilddog configuration script

--- test/wilddog_config.py
+++ test/wilddog_config.py
@@ -1,6 +1,6 @@
 from leancloud import init
 import requests
-import json
+from create_fake_data import get_userid_list
 
 
 def post_panel_data(**param):
@@ -11,14 +11,16 @@ def post_panel_data(**param):
                "X-AVOSCloud-Application-Key": "6z6n0w3dopxmt32oi2eam2dt0orh8rxnqc8lgpf2hqnar4tr"}
 
     payload = {"userId": user_id, "config": config}
-    rep = requests.post("http://leancloud.cn/1.1/functions/notify_new_config",  headers=headers, json=payload)
+    print(payload)
+    rep = requests.post("https://leancloud.cn/1.1/functions/notify_new_config",  headers=headers, json=payload)
     print(rep.content)
 
 if __name__ == '__main__':
-    init('pin72fr1iaxb7sus6newp250a4pl2n5i36032ubrck4bej81',
-         'qs4o5iiywp86eznvok4tmhul360jczk7y67qj0ywbcq35iia')
+    init('2x27tso41inyau4rkgdqts0mrao1n6rq1wfd6644vdrz2qfo',
+         '3fuabth1ar3sott9sgxy4sf8uq31c9x8bykugv3zh7eam5ll')
 
-    user_id = "55b3ccd940ac21f9f969680a"
+    # user_id = "55b3ccd940ac21f9f969680a"
+    tracker_list = get_userid_list()
     config = {
         "period": 30,
         "collector": {
@@ -54,5 +56,6 @@ if __name__ == '__main__':
             }
         }
     }
-
-    post_panel_data(user_id=user_id, config=config)
+    for user_id in tracker_list:
+        print(user_id)
+        post_panel_data(user_id=user_id, config=config)
