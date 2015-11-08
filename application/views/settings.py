9cc730381520fa39a17786e18f99617e772dfb1e
mageia <yzg963@gmail.com>
2015-11-08 16:51:55 +0800

    settings pages

--- application/views/settings.py
+++ application/views/settings.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, session, request, jsonify, redirect, url_for
+from flask import Blueprint, render_template, session, request, redirect, url_for
 from ..models import Developer
 
 
