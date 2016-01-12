732bdcc22a14e6f5abab46b7773df21038ec1624
mageia <yzg96@gmail.com>
2016-01-12 16:44:56 +0800

    translate

--- application/views/dashboard.py
+++ application/views/dashboard.py
@@ -947,6 +947,7 @@ def get_attr_of_user(uid, h_start=None, h_end=None, e_start=None, e_end=None, wo
         x['motion_count'] = dict(map(lambda y: (translate(translate(y, "motion_old"), "motion"),
                                                 motion_count.get(y)), motion_count.keys()))
         x['label'] = translate(translate(x.get('label') or "", "event_old"), "home_office_status_old")
+        # print x['label'], x['motion_count']
         detailData.append(x)
     ret_dict['detailData'] = detailData
     return ret_dict
