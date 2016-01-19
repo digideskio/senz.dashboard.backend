1c87c731e1940cb338551790b29c78444c6136a0
mageia <yzg96@gmail.com>
2016-01-19 13:48:05 +0800

    contextEntertaining

--- application/views/dashboard.py
+++ application/views/dashboard.py
@@ -484,6 +484,7 @@ def get_tracker_of_app(app_id=None, group_id=None):
                 u'560d3a9960b2ad8a22f32966', u'564bd84b60b2ed362064985f', u'55d845e100b0d7b2266ac668',
                 u'564575ac60b20fc9b99d8d9d', u'56065bba60b2aac0d6f2a38a', u'558a5ee7e4b0acec6b941e96',
                 u'55f788f4ddb25bb7713125ef', u'5588d20be4b0dc547bacb2ce', u'5653c88e00b0e772838cd61b',
+                u'565d52af60b2824f23970155', u'568a0ca200b01b9f2c08f53d', u'55c1e2d900b0ee7fd66e8ea3',
                 u'5682580d00b0f9a1f22748c7', u'5684d3d660b2b60f65d84285']
     app = {
         "__type": "Pointer",
