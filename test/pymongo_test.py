1ca25c4c5462ac43acf1e03f168f74661b86f500
mageia <yzg96@gmail.com>
2016-01-20 19:30:59 +0800

    contextEntertaining

--- test/pymongo_test.py
+++ test/pymongo_test.py
@@ -1,7 +1,18 @@
 from pymongo import *
+import requests
+import datetime
+import json
 
-client = MongoClient('mongodb://senzhub:Senz2everyone@119.254.111.40/RefinedLog')
-target = client.get_default_database().CombinedTimelines
+# client = MongoClient('mongodb://senzhub:Senz2everyone@119.254.111.40/RefinedLog')
+# target = client.get_default_database().CombinedTimelines
+#
+# for i in target.find({}):
+#     print i
 
-for i in target.find({}):
-    print i
+url = "http://0.0.0.0:3000/api/UserEvents"
+data = {
+    "test": str(datetime.datetime.fromtimestamp(1453279688))
+}
+
+print requests.post(url=url, json=json.dumps(data))
+
