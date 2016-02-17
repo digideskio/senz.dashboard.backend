d7d32c9f81e8a040b4230f2151b220d1b4c8f685
like <m15652768278@163.com>
2016-02-17 17:55:41 +0800

    fix bug

--- application/static/scripts/dashboard.js
+++ application/static/scripts/dashboard.js
@@ -3,17 +3,33 @@ $(function(){
     $navItem = $("#sidebar").find(".sub-nav li");
 
     if(path.indexOf("profile") > 0) {
-        $navItem.eq(0).addClass("active");  
+        $navItem.eq(0).addClass("active");
+        // $("#userGroupNav").css("color","rgba(51, 204, 204, 1)");
+        $("#userGroupSubNav").css("display","block");
     } else if (path.indexOf("interest") > 0) {
         $navItem.eq(1).addClass("active"); 
+        $("#userGroupSubNav").css("display","block");
     } else if (path.indexOf("marriage") > 0) {
         $navItem.eq(2).addClass("active");
+        $("#userGroupSubNav").css("display","block");
     } else if (path.indexOf("consumption") > 0) {
         $navItem.eq(3).addClass("active");
+        $("#userGroupSubNav").css("display","block");
     } else if (path.indexOf("location") > 0) {
         $navItem.eq(4).addClass("active");
+        $("#userGroupSubNav").css("display","block");
     } else if (path.indexOf("motion") > 0) {
         $(".motion").addClass("active");
+    } else if (path.indexOf("context") > 0) {
+        $(".context").addClass("active");
+    } else if (path.indexOf("single") > 0) {
+        $(".single").addClass("active");
+    } else if (path.indexOf("group") > 0) {
+        $(".group").addClass("active");
+    } else if (path.indexOf("push") > 0) {
+        $(".push").addClass("active");
+    } else if (path.indexOf("history") > 0) {
+        $(".history").addClass("active");
     } else {
         $(".index").addClass("active");
     }
